f0656e34eeb360f7a9c23ab71afb25330ae2f1c9
AidenCarere <aidencarere@gmail.com>
2024-04-10 17:35:00 -0700

    Installed MaterialUI, used it to add Next and Previous button in a new component file called Button.jsx & rendered to HomePage

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function ButtonUsage() {
+  return (
+  <div>
+  <Button variant="contained">Next</Button>
+  <Button variant="contained">Previous</Button>
+  </div>
+)
+};
